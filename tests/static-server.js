9bd362be943558fa20f9aa67580bb16ae7721b07
LuisCaceres <luiscaceresmx@gmail.com>
2024-09-12 18:14:16 +1000

    Refactor: Create a html file to run unit tests
    This commit modifies where unit tests are stored and how unit tests are run in the browser. The location of the static.server.js and tests.html files has changed. The contents of those files have been updated accordingly.

--- tests/static-server.js
+++ tests/static-server.js
@@ -3,29 +3,36 @@ browser. Open the parent folder in the terminal and run the following command:
 `node static-server.js`. The server will be running at http://localhost:8000/.
 */
 
-const fs = require("node:fs");
-const http = require("node:http");
-const path = require("node:path");
+const fs = require('node:fs');
+const http = require('node:http');
+const path = require('node:path');
 
 const port = 8000;
 
 const MIME_TYPES = {
-  html: "text/html",
-  js: "text/javascript",
+  default: 'application/octet-stream',
+  html: 'text/html; charset=UTF-8',
+  js: 'application/javascript',
+  css: 'text/css',
+  png: 'image/png',
+  jpg: 'image/jpg',
+  gif: 'image/gif',
+  ico: 'image/x-icon',
+  svg: 'image/svg+xml',
 };
 
-const STATIC_PATH = path.join(process.cwd(), "../../");
+const STATIC_PATH = path.join(process.cwd(), '../');
 
 const toBool = [() => true, () => false];
 
 const prepareFile = async (url) => {
   const paths = [STATIC_PATH, url];
-  if (url.endsWith("/")) paths.push("src/test/tests.html");
+  if (url.endsWith('/')) paths.push('tests/tests.html');
   const filePath = path.join(...paths);
   const pathTraversal = !filePath.startsWith(STATIC_PATH);
   const exists = await fs.promises.access(filePath).then(...toBool);
   const found = !pathTraversal && exists;
-  const streamPath = found ? filePath : STATIC_PATH + "/404.html";
+  const streamPath = found ? filePath : STATIC_PATH + '/404.html';
   const ext = path.extname(streamPath).substring(1).toLowerCase();
   const stream = fs.createReadStream(streamPath);
   return { found, ext, stream };
@@ -36,7 +43,7 @@ http
     const file = await prepareFile(req.url);
     const statusCode = file.found ? 200 : 404;
     const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
-    res.writeHead(statusCode, { "Content-Type": mimeType });
+    res.writeHead(statusCode, { 'Content-Type': mimeType });
     file.stream.pipe(res);
     console.log(`${req.method} ${req.url} ${statusCode}`);
   })
