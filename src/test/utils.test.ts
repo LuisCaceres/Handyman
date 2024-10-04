8da39f25ed0326cbfcbbbf5ec8f0301306090586
LuisCaceres <luiscaceresmx@gmail.com>
2024-10-04 19:08:37 +1000

    Feature: Create methods isSingular() and toPlural() for Word class
    This commit creates the following methods for a Word class:
    * A method that indicates whether a word is in singular form.
    * A method that returns the plural form of a word.

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -3,6 +3,76 @@ import * as utils from '../utils.js';
 describe('Word', function () {
   const Word = utils.Word;
 
+  describe('#isSingular()', function () {
+    it('It indicates whether a word is in singular form.', function () {
+      const value1 = (new Word('box')).isSingular();
+      const value2 = (new Word('church')).isSingular();
+      const value3 = (new Word('class')).isSingular();
+      const value4 = (new Word('country')).isSingular();
+      const value5 = (new Word('dog')).isSingular();
+      const value6 = (new Word('entry')).isSingular();
+      const value7 = (new Word('lie')).isSingular();
+      const value8 = (new Word('pie')).isSingular();
+      const value9 = (new Word('service')).isSingular();
+
+      chai.expect(value1).to.be.true;
+      chai.expect(value2).to.be.true;
+      chai.expect(value3).to.be.true;
+      chai.expect(value4).to.be.true;
+      chai.expect(value5).to.be.true;
+      chai.expect(value6).to.be.true;
+      chai.expect(value7).to.be.true;
+      chai.expect(value8).to.be.true;
+      chai.expect(value9).to.be.true;
+    });
+  });
+
+  describe('#toPlural()', function () {
+    it('It returns the plural form of a word in singular form.', function () {
+        const value1 = (new Word('box')).toPlural();
+        const value2 = (new Word('church')).toPlural();
+        const value3 = (new Word('class')).toPlural();
+        const value4 = (new Word('country')).toPlural();
+        const value5 = (new Word('dog')).toPlural();
+        const value6 = (new Word('entry')).toPlural();
+        const value7 = (new Word('lie')).toPlural();
+        const value8 = (new Word('pie')).toPlural();
+        const value9 = (new Word('service')).toPlural();
+  
+        chai.expect(value1).to.equal('boxes');
+        chai.expect(value2).to.equal('churches');
+        chai.expect(value3).to.equal('classes');
+        chai.expect(value4).to.equal('countries');
+        chai.expect(value5).to.equal('dogs');
+        chai.expect(value6).to.equal('entries');
+        chai.expect(value7).to.equal('lies');
+        chai.expect(value8).to.equal('pies');
+        chai.expect(value9).to.equal('services');
+    });
+
+    it('It returns the plural form of a word in plural form.', function () {
+      const value1 = (new Word('boxes')).toPlural();
+      const value2 = (new Word('churches')).toPlural();
+      const value3 = (new Word('classes')).toPlural();
+      const value4 = (new Word('countries')).toPlural();
+      const value5 = (new Word('dogs')).toPlural();
+      const value6 = (new Word('entries')).toPlural();
+      const value7 = (new Word('lies')).toPlural();
+      const value8 = (new Word('pies')).toPlural();
+      const value9 = (new Word('services')).toPlural();
+
+      chai.expect(value1).to.equal('boxes');
+      chai.expect(value2).to.equal('churches');
+      chai.expect(value3).to.equal('classes');
+      chai.expect(value4).to.equal('countries');
+      chai.expect(value5).to.equal('dogs');
+      chai.expect(value6).to.equal('entries');
+      chai.expect(value7).to.equal('lies');
+      chai.expect(value8).to.equal('pies');
+      chai.expect(value9).to.equal('services');
+    });
+  });
+
   describe('#toSingular()', function () {
     it('Returns the singular form of a word.', function () {
        const value1 = (new Word('boxes')).toSingular();
--- src/utils.ts
+++ src/utils.ts
@@ -11,6 +11,14 @@ class Word extends String {
     }
 
     /**
+     * Indicates whether this word is in singular form.
+     * @returns {boolean} True if the word is singular; otherwise, false.
+     */
+    isSingular() {
+        return this.endsWith('ss') || this.endsWith('s') === false;
+    }
+
+    /**
      * Returns the singular form of this word. 
      * @return {string} The singular form of this word.
      */
@@ -40,6 +48,39 @@ class Word extends String {
 
         return wordInSingular;
     }
+
+    /**
+     * Returns the plural form of this word. 
+     * @return {string} The plural form of this word.
+     */
+    toPlural() {
+        let wordInPlural;
+
+        switch (true) {
+            case this.endsWith('y'):
+                // For example: 'entry' and 'country'.
+                wordInPlural = `${this.slice(0, -1)}ies`;
+                break;
+            case this.endsWith('ch'):
+            case this.endsWith('ss'):
+            case this.endsWith('x'):
+                // For example: 'box', 'church' and 'class'.
+                wordInPlural = `${this}es`;
+                break;
+            // Word is already in plural form.
+            case this.endsWith('s'):
+                // For example: 'dogs' and 'pies'.
+                wordInPlural = `${this}`;
+                break;
+            default:
+                // For example: 'dog' and 'pie'.
+                wordInPlural = `${this}s`;
+                break;
+        }
+
+        return wordInPlural;
+    }
+}
 }
 
 export {
