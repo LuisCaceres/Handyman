8631e74aedc7ddd7d659aa9cec6243e8b48aca93
LuisCaceres <luiscaceresmx@gmail.com>
2025-10-09 22:07:31 +1100

    Refactor: Simplify completion item provider by removing code
    Before, the logic of creating code completions was placed in this one file. However, there are many many scenarios that can't be handled equally with code.
    
    Now, the logic for each of the scenarios for code completion is loaded by this file instead. The logic is then kept separate.

--- src/completionItemProvider.ts
+++ src/completionItemProvider.ts
@@ -2,16 +2,12 @@
 
 // The module 'vscode' contains the VS Code extensibility API.
 import * as vscode from "vscode";
-import { regexes } from "./utils.js";
-import { foo } from "./languageTokenizer.js";
-
-foo;
-
-// Let `dataSets` be a list of data sets each of which containing information about code snippets.
-import { getRelevantCodeSnippets } from "./codeActionsProviderData.js";
 
 // Supported language types.
 const languages = ["javascript", "typescript", "vue"];
+
+// Let `functions` be a list of functions that get code completions.
+import { functions } from "./completionItemProviderData.js";
 
 /**
  * Provide a list of completion items given `position` and characters the developer is typing.
@@ -19,54 +15,15 @@ const languages = ["javascript", "typescript", "vue"];
  * @param {vscode.Range} position - The current position of the caret within `file`.
  * @returns {vscode.CompletionItem[]} - A list of code completions.
  */
-function provideCompletionItems(file: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.CompletionItem[] {
+async function provideCompletionItems(file: vscode.TextDocument,
+    position: vscode.Position, token: vscode.CancellationToken,
+    context: vscode.CompletionContext): Promise<vscode.CompletionItem[]> {
+
     // Let `completionItems` be an initially empty list of completion items.
     const completionItems: vscode.CompletionItem[] = [];
 
-    const lines = {
-        current: file.lineAt(position.line),
-        // TO DO: GET PREVIOUS NON EMTPY LINE OF TEXT
-        previous: file.lineAt(position.line - 1),
-        next: file.lineAt(position.line + 1)
-        // TO DO: GET NEXT NON EMPTY LINE OF TEXT
-    };
-
-    // Let `currentText` be the text of the line on which the caret is located.
-    const currentText = lines.current.text.trim();
-    // Let `nextText` be the text of the line on which the caret is located.
-    const nextText = lines.next.text.trim();
-
-    // Let `words` be variable names that the previous line has.
-    const words = new Set(lines.previous.text.trim().match(regexes.identifier) || []);
-
-    // If there are words in `words`.
-    if (words?.size) {
-
-        // For each `word` in `words`.
-        for (const word of words) {
-            // Let `completionItem` be a new completion item.
-            const completionItem: vscode.CompletionItem = {
-                insertText: `console.log('${word}:\\n', ${word});`,
-                kind: 14,
-                label: `log(${word})`,
-                sortText: word,
-            };
-
-            // Add `completionItem` to `completionItems`.
-            completionItems.push(completionItem);
-
-            /*${ if (currentText.startsWith('//') && nextText.startsWith('const')) {
-                    const variable = nextText.match(/(?<=const\s)\w+/)?.[0] || '';
-                    // Let `completionItem` be a new completion item.
-                    const completionItem: vscode.CompletionItem = {
-                        insertText: ` Let \`${variable}\` be `,
-                        label: '',
-                    };
-
-                    // Add `completionItem` to `completionItems`.
-                    completionItems.push(completionItem);
-                }}*/
-        }
+    for (const fnction of functions) {
+        completionItems.push(...await fnction(file, position));
     }
 
     return completionItems;
